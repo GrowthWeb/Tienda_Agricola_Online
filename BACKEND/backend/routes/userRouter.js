33e8682fb0c2e850865423934dd0e35285946551
JhonMeza96 <estebanmezaher96@gmail.com>
2022-11-21 07:38:39 -0500

    Actualizaciones en backend

--- BACKEND/backend/routes/userRouter.js
+++ BACKEND/backend/routes/userRouter.js
@@ -1,16 +1,15 @@
 import { Router } from "express";
-import { verifyAcces } from "../conections/license.js";
-import { createUsuario, deleteUsuario, getUsuario, getUsuarios, login, updateUsuario} from "../controllers/userController.js";
+
+import { createUsuario, deleteUsuario, getUsuario, getUsuarios, updateUsuario} from "../controllers/userController.js";
 
 
 const router= Router();
 
-router.get('/usuarios', verifyAcces, getUsuarios);
+router.get('/usuarios', getUsuarios);
 router.post('/usuarios', createUsuario);
 router.put('/usuarios/:id', updateUsuario);
 router.delete('/usuarios/:id', deleteUsuario);
 
 router.get('/usuarios/:id', getUsuario);
-router.post('/login/',login);
 
 export default router
